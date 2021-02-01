2168bbe0cbcc0be1e16511648feb8a97877482a8
SABBIR <sabbirto13@gmail.com>
2021-02-01 18:56:31 +0600

    variables

--- let_const_array_object_Declared.js
+++ let_const_array_object_Declared.js
@@ -1,1 +1,26 @@
+/* const Variables */
+const hubby = "Mahfuza Shorna";
+// hubby = "Jannatul"; -error
+console.log(hubby);
+
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(13);
+// numbers = ["Komol", "Salkin", "Mizan"]; -error
+const nayok = {name: "Sakib Khan", phone: 08801829};
+console.log(numbers);
+console.log(nayok);
+
+
+/* let Variables */
+let patientName = "Shafa";
+patientName = "Zafrin";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
 
